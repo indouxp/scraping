941801168a78339a24143a31b7b0736872d988aa
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-09-19 13:26:24 +0900

    update spl061.rb

--- spl061.rb
+++ spl061.rb
@@ -13,16 +13,33 @@ begin
   driver.get html
   elm_body = driver.find_element(:id, "top")
   elm_tables = elm_body.find_elements(:xpath, "//table")
+  elm_table = nil
   elm_tables.each do |table|
-    recs = table.find_elements(:xpath, "//tr")
-    row_no = 0
-    recs.each do |rec|
-      unless row_no == 0
-        printf "\nNo. %02d\n", row_no
-        puts rec.text
-      end
-      row_no += 1
+    classes = table.attribute("class") &&
+    if /jobs/ =~ classes & /mixin-for-list/ =~ classes
+      elm_table = table
+      break
     end
+  end
+  recs = elm_table.find_elements(:xpath, "//tr")
+  row_no = 0
+  recs.each do |rec|
+    #unless row_no == 0
+    if row_no == 1
+      printf "\nNo. %02d\n", row_no
+      puts rec.text
+      data_no = 0
+      datas = rec.find_elements(:xpath, "//td")
+      puts datas.size
+      datas.each do |data|
+        printf "\nNo. %02d-%02d\n", row_no, data_no
+        puts "class:[#{data.attribute("class")}]"
+        puts "text:[#{data.text}]"
+        data_no += 1
+      end
+      break
+    end
+    row_no += 1
   end
 
 rescue => eval
