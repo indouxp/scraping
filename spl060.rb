38b2f7dc28fbff73e8495b678af844376ff670b3
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-09-20 22:06:52 +0900

    update spl06x.rb

--- spl060.rb
+++ spl060.rb
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 require "selenium-webdriver"
+$debug = false
 
 class Crowdworks
+  attr_reader :done
+
   def initialize
     @driver = Selenium::WebDriver.for :chrome
+    STDERR.puts @driver if $debug
     @base_url = "http://crowdworks.jp/"
     @driver.manage.timeouts.implicit_wait = 30
+    @done = false
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
   end
 
   def login(username, password)
@@ -19,9 +25,56 @@ class Crowdworks
     @driver.find_element(:css, "span").click
     @driver.find_element(:link, "開発").click
     @driver.find_element(:css, "option[value=\"-expires\"]").click
+    STDERR.puts @driver if $debug
   end
 
-  def done
+  def report
+    elm_body = @driver.find_element(:id, "top")
+    elm_tables = elm_body.find_elements(:xpath, "//table")
+    begin
+      elm_table = nil
+      elm_tables.each do |table|
+        classes = table.attribute("class")
+        if /jobs/ =~ classes && /mixin-for-list/ =~ classes
+          elm_table = table
+          break
+        end
+      end
+      raise "ERR01" if elm_table == nil
+    rescue => eval
+      raise eval
+    end
+    STDERR.puts "elm_tables:#{elm_tables}" if $debug
+    datas = elm_table.find_elements(:xpath, "//tr/td")
+    STDERR.puts "datas:#{datas}" if $debug
+    record_no = 0
+    data_no = 0
+    datas.each do |data|
+      if data.attribute("class") == "title"
+        record_no += 1
+        data_no = 1
+      end
+      if data.attribute("class") == "title"
+        printf "\nNo. %02d-%02d\n", record_no, data_no  if      $debug
+        printf "\nNo. %02d\n", record_no                unless  $debug
+      end
+      puts "#{data.attribute("class")}:#{data.text.gsub(/\r\n|\r|\n/, "")}"
+      data_no += 1
+      if data.attribute("class") == "expired_on" &&
+        /募集終了/ =~ data.text
+        @done = true
+        break
+      end
+    end
+  end
+
+  def next_page
+    elm_next_page = @driver.find_element(:link, "次のページ →")
+    elm_next_page.click
+    STDERR.puts elm_next_page if $debug
+  end
+
+  def quit
     @driver.quit
   end
 end
@@ -31,9 +84,14 @@ begin
   username = ARGV[0]
   password = ARGV[1]
   crowdworks.login(username, password)
+  until crowdworks.done 
+    crowdworks.report
+    crowdworks.next_page unless crowdworks.done
+  end
+  
 rescue => eval
-  STDERR.puts eval.backtrace.join("\n")
+  STDERR.puts eval.backtrace.join("\n") if $debug
   STDERR.puts eval
 ensure
-  #crowdworks.done
+  #crowdworks.quit
 end
--- spl061.rb
+++ spl061.rb
@@ -1,4 +1,4 @@
-# *-* coding:utf-8 *-*
+# coding: utf-8
 # クラウドワークス
 require "selenium-webdriver"
 $debug = true
@@ -26,26 +26,14 @@ begin
   rescue => eval
     raise eval
   end
-  recs = elm_table.find_elements(:xpath, "//tr")
-  row_no = 0
-  recs.each do |rec|
-    unless row_no == 0
-    #if row_no == 1
-      printf "\nNo. %02d\n", row_no
-      puts rec.text
-      datas = rec.find_elements(:xpath, "//td")
-      #puts datas.size
-      data_no = 0
-      datas.each do |data|
-        printf "\nNo. %02d-%02d/%02d\n", row_no, data_no, datas.size
-        puts "class:[#{data.attribute("class")}]"
-        puts "text:[#{data.text.gsub(/\r\n|\r|\n/, "")}]"
-        data_no += 1
-      end
-    end
-    row_no += 1
+  datas = elm_table.find_elements(:xpath, "//tr/td")
+  data_no = 0
+  datas.each do |data|
+    printf "\nNo. %02d/%02d\n", data_no, datas.size
+    puts "class:[#{data.attribute("class")}]"
+    puts "text:[#{data.text.gsub(/\r\n|\r|\n/, "")}]"
+    data_no += 1
   end
-
 rescue => eval
   STDERR.puts eval.backtrace.join("\n")
   STDERR.puts eval
