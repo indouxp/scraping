79eba023f2db80c9b8ce7a7f6b71f553814ef4cb
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-04-11 19:07:22 +0900

    update crowdworks4.rb

--- crowdworks4.rb
+++ crowdworks4.rb
@@ -38,13 +38,117 @@ def main
   #doc = Nokogiri::HTML.parse(html, nil, 'UTF-8')
   #puts html
   #puts "-----"
-  lis = driver.find_elements(:xpath => '//*[@id="result_jobs"]/div[2]/div/ul/li')
-  lis.size.times do | count |
-    driver.find_element(:xpath => '//*[@id="result_jobs"]/div[2]/div/ul/li[' + "#{count + 1}" + ']')
-    puts "ok:#{count + 1}"
+  base_url = "https://crowdworks.jp"
+  no = 0
+  done = false
+  until done
+    lis = driver.find_elements(:xpath => '//*[@id="result_jobs"]/div[2]/div/ul/li')
+    lis.size.times do | count |
+      job = driver.find_element(:xpath => '//*[@id="result_jobs"]/div[2]/div/ul/li[' + "#{count + 1}" + ']')
+      if job.tag_name == 'li'
+        ref = job.attribute('data-href')
+        text = job.text.gsub(/気になる！リストに追加\n?/, "")
+      else
+        raise "tag_name is not 'li'"
+      end
+      if /募集終了/ =~ job.text
+        done = true
+        break
+      end
+      no += 1
+      puts "====================================#{no}"
+      puts text
+      puts "#{base_url}/#{ref}"
+    end
+    unless done
+      driver.find_element(:class => 'to_next_page').click
+    end
   end
-  return driver
+  driver.quit
 end
+
+=begin
+[:inspect
+ :==
+ :eql?
+ :hash
+ :click
+ :tag_name
+ :attribute
+ :text
+ :send_keys
+ :send_key
+ :clear
+ :enabled?
+ :selected?
+ :displayed?
+ :submit
+ :css_value
+ :style
+ :location
+ :location_once_scrolled_into_view
+ :size
+ :first
+ :all
+ :[]
+ :ref
+ :to_json
+ :as_json
+ :find_element
+ :find_elements
+ :nil?
+ :===
+ :=~
+ :!~
+ :<=>
+ :class
+ :singleton_class
+ :clone
+ :dup
+ :taint
+ :tainted?
+ :untaint
+ :untrust
+ :untrusted?
+ :trust
+ :freeze
+ :frozen?
+ :to_s
+ :methods
+ :singleton_methods
+ :protected_methods
+ :private_methods
+ :public_methods
+ :instance_variables
+ :instance_variable_get
+ :instance_variable_set
+ :instance_variable_defined?
+ :remove_instance_variable
+ :instance_of?
+ :kind_of?
+ :is_a?
+ :tap
+ :send
+ :public_send
+ :respond_to?
+ :extend
+ :display
+ :method
+ :public_method
+ :define_singleton_method
+ :object_id
+ :to_enum
+ :enum_for
+ :equal?
+ :!
+ :!=
+ :instance_eval
+ :instance_exec
+ :__send__
+ :__id__]
+
+=end
+
 
 def login(driver, login_url, user, pass, confirm)
   begin
