353e82c2c5c72d100cdd170b79d6891491dc3e64
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-04-25 21:14:49 +0900

    update cpubenchmark0.sh

--- cpubenchmark0.sh
+++ cpubenchmark0.sh
@@ -2,15 +2,21 @@
 if [ ! -f cpubenchmark0.txt ]; then
   ruby cpubenchmark0.rb > cpubenchmark0.txt
 fi
-select=$1
-shift
-for i in "$@"
-do
-  select="${select}|$i"
-done
-  
-select="${select}|Core i7 920 "
+select="Core i7 920 "
 select="${select}|Phenom II X4 9[46]5"
 select="${select}|Xeon E3-1230 @ 3.20GHz"
 select="${select}|Core i5-2415M @ 2.30GHz"
+select="${select}|Core i7-2677M @ 1.80GHz"
+select="${select}|Core2 Duo T8300"
+select="${select}|Athlon 64 X2 Dual Core 6000+"
+select="${select}|Core Solo T1300"
+select="${select}|Core2 Duo U9400"
+
+if [ "$#" -ne "0" ]; then
+  for i in "$@"
+  do
+    select="${select}|$i"
+  done
+fi
+  
 egrep "(${select})" cpubenchmark0.txt | sort
