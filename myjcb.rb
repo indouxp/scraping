787900c576b7fc21ed71c79cd6a07707f03676a8
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2015-01-09 13:17:49 +0900

    MyJCBのメニュー画面にまだ進めない

--- myjcb.rb
+++ myjcb.rb
@@ -1,7 +1,10 @@
 # encoding:utf-8
+# myjcbにログインし、利用明細を取得する
+#
 require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
+require 'yaml'
 
 Capybara.run_server = false
 Capybara.current_driver = :selenium
@@ -11,17 +14,27 @@ module Crawler
   class Myjcb
     include Capybara::DSL
 
-    def login
+    def login(user, pass)
       visit('/')
       fill_in "userId",
-        :with => 'ecu39979'
+        :with => user
       fill_in "password",
-        :with => 'InTatsu1'
+        :with => pass
       click_button "ログイン"
+      sleep 5                           # 一時的
+      click_link "MyJCBメニュー画面へ"  # ２つリンクがあるのでエラー
     end
   end
 end
 
-crawler = Crawler::Myjcb.new
-crawler.login
+if __FILE__ == $0
+  yaml_file = File.dirname($0) + "/" + "myjcb.txt"
+  data = YAML.load_file(yaml_file)
+  user = data["user"]
+  pass = data["pass"]
+
+  crawler = Crawler::Myjcb.new
+  crawler.login(user, pass)
+  exit 0
+end
 
